45abb24bedd4749de1d0aa7a2645cc2503a4b560
ArchaSoman <120626585+ArchaSoman@users.noreply.github.com>
2023-01-18 09:24:13 +0000

    Delete 3co1.py

--- 3co1.py
+++
@@ -1,7 +0,0 @@
-import random
-mylist =['Selva','Roshni','Pablo','Sitara','jai','Fugi']
-print(random.choice(mylist))
-print(random.sample(mylist,k=2))
-print(random.shuffle(mylist))
-print(mylist)
-print(random.randrange(3,9))
