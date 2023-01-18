6d28d9470bcae5d3cc1a2fed0968c964c16626af
ArchaSoman <120626585+ArchaSoman@users.noreply.github.com>
2023-01-18 06:40:54 +0000

    Add files via upload

---
+++ 3co5.py
@@ -0,0 +1,8 @@
+import statistics as s
+list1=[20,30,40,50,60]
+print("Mean:",s.mean(list1))
+print("Median:",s.median(list1))
+print("Mode:",s.mode(list1))
+print("Harmonic Mean:",s.harmonic_mean(list1))
+print("Statistic Variance:",s.variance(list1))
+print("Statistic Median Low:",s.median_low([-12,6.6,-3,4,6,7.1,-9,22]))
