e6c9e2f0581282be91537d9ec19bd4eef4cfbc44
ArchaSoman <120626585+ArchaSoman@users.noreply.github.com>
2023-01-18 09:30:25 +0000

    Add files via upload

---
+++ Cycle 3 Pgm/3co1.py
@@ -0,0 +1,7 @@
+import random
+mylist =['Selva','Roshni','Pablo','Sitara','jai','Fugi']
+print(random.choice(mylist))
+print(random.sample(mylist,k=2))
+print(random.shuffle(mylist))
+print(mylist)
+print(random.randrange(3,9))
