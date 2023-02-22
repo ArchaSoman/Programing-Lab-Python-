b0a54793415c94b7fb692c97275ac48e6ea3527f
ArchaSoman <120626585+ArchaSoman@users.noreply.github.com>
2023-02-22 14:09:37 +0530

    Add files via upload

---
+++ Cycle 3 Pgm/graphics/threedgraphics/cuboid.py
@@ -0,0 +1,5 @@
+def cuboidarea(l,b,h):
+    return((2*l*b)+(2*l*h)+(2*b*h))
+def cuboidperimeter(l,b,h):
+    return(4*(l+b+h))
+    
---
+++ Cycle 3 Pgm/graphics/threedgraphics/sphere.py
@@ -0,0 +1,5 @@
+from math import pi
+def spherearea(r):
+    return(4*pi*r*r)
+def sphereperimeter(r):
+    return(2*pi*r)
